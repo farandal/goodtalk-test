08ce64ce3a398e0ecf53cdda74a2cd22922bb65d
Francisco Aranda <faranda@gmail.com>
2019-02-20 10:56:38 -0300

    BE - fix a few typos issues in backend

--- backend/services/items/index.js
+++ backend/services/items/index.js
@@ -21,9 +21,7 @@
 import Item, { ItemSchema } from '../../models/Item';
 
 export const getItemsList = () => {
-  
   return new Promise((resolve, reject) => {
-    
     Item.find().exec((err, items) => {
       if (err) {
         console.log(err);
@@ -37,9 +35,7 @@ export const getItemsList = () => {
 
       resolve(items.map(item => item.view(false)));
     });
-
   });
-
 };
 
 /**
@@ -49,43 +45,35 @@ export const getItemsList = () => {
  * @param {Object} resource Item resource
  * @return {Object} item Iserted Item JSON object
  */
-export const insertItem = (resource) => {
- 
+export const insertItem = resource => {
   const object = {
     name: resource.name,
     description: resource.description
   };
 
   return new Promise((resolve, reject) => {
-    
     const item = new Item(object);
 
     try {
-
       item.save((err, item) => {
         if (err) {
           reject({
             code: 409,
             message: 'There was an error creating the Item',
-            error: error
+            error: err
           });
         }
         resolve(item);
       });
-
-    } catch(error) {
-
+    } catch (error) {
       console.log(error);
       reject({
         code: 409,
         message: 'There was an error creating the Item',
         error: error
       });
-
     }
-
   });
-
 };
 
 /**
@@ -95,12 +83,9 @@ export const insertItem = (resource) => {
  * @param {String} itemId itemId
  * @return {Object} item JSON object
  */
-export const getItem = (itemId) => { 
-
-  return new Promise((resolve, reject) => { 
-
+export const getItem = itemId => {
+  return new Promise((resolve, reject) => {
     Item.findById(itemId, (err, item) => {
-
       if (err) {
         console.log(err);
         reject({
@@ -111,12 +96,8 @@ export const getItem = (itemId) => {
         return;
       }
       resolve(item.view(true));
-
     });
-
   });
-
-
 };
 
 /**
@@ -127,14 +108,10 @@ export const getItem = (itemId) => {
  * @param {Object} resource Item JSON object
  * @return {Object} Updated Item JSON object
  */
-export const updateItem = ( itemId, resource) => {
-
+export const updateItem = (itemId, resource) => {
   return new Promise((resolve, reject) => {
-    
     Item.findById(itemId, (err, item) => {
-
       if (err || !item) {
-
         console.log(err);
 
         reject({
@@ -144,27 +121,23 @@ export const updateItem = ( itemId, resource) => {
         });
 
         return;
-
       }
 
       console.log(item);
 
-      if(resource && resource.name && resource.name != "") {
+      if (resource && resource.name && resource.name != '') {
         item.name = resource.name;
       }
-      
-      if(resource && resource.description && resource.description != "") {
+
+      if (resource && resource.description && resource.description != '') {
         item.description = resource.description;
       }
-   
-      item.save( (err, response) => { 
-          resolve(response);
+
+      item.save((err, response) => {
+        resolve(response);
       });
-
     });
-
   });
-
 };
 
 /**
@@ -175,12 +148,9 @@ export const updateItem = ( itemId, resource) => {
  * @return {Object} object with a message containing 'Resource has been deleted'
  */
 
-export const deleteItem = (itemId) => {
- 
+export const deleteItem = itemId => {
   return new Promise((resolve, reject) => {
-    
     Item.findByIdAndRemove(itemId, (err, response) => {
-
       if (err) {
         console.log(err);
         reject({
@@ -193,8 +163,6 @@ export const deleteItem = (itemId) => {
       resolve({
         message: 'Item has been deleted'
       });
-
     });
-
   });
 };
